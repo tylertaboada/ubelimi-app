6085aa75e715347bcb4da59f0afeee08290c3c9d
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-08-03 14:10:41 +0100

    creted all remaining views

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@
 const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
+/*
+const generateRandomToken = length => {
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let token = '';
+  for (let i = 0; i < length; i++) {
+    token += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return token;
+};
 
+const nodemailer = require('nodemailer');
+
+
+const transport = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASSWORD
+  }
+});
+*/
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello World!' });
 });
