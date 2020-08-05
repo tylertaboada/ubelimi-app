916af6102b938618ec1354783c317c7503b77372
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-08-05 09:46:22 +0100

    updated maap on views

--- routes/moment.js
+++ routes/moment.js
@@ -19,42 +19,30 @@ const storage = new multerStorageCloudinary.CloudinaryStorage({
 });
 const upload = multer({ storage });
 
-momentRouter.post(
-  '/create',
-  routeGuard,
-  upload.single('photo'),
-  (req, res, next) => {
-    const {
-      feeling,
-      description,
-      learning,
-      gratitude,
-      latitude,
-      longitude
-    } = req.body;
+momentRouter.post('/create', routeGuard, upload.single('photo'), (req, res, next) => {
+  const { feeling, description, learning, gratitude, latitude, longitude } = req.body;
 
-    let url;
-    if (req.file) {
-      url = req.file.path;
-    }
-
-    Moment.create({
-      creator: req.session.passport.user,
-      feeling: feeling,
-      description: description,
-      learning: learning,
-      gratitude: gratitude,
-      photo: url,
-      location: { coordinates: [latitude, longitude] }
-    })
-      .then(moment => {
-        res.redirect(`/moment/${moment._id}`);
-      })
-      .catch(error => {
-        next(error);
-      });
+  let url;
+  if (req.file) {
+    url = req.file.path;
   }
-);
+
+  Moment.create({
+    creator: req.session.passport.user,
+    feeling: feeling,
+    description: description,
+    learning: learning,
+    gratitude: gratitude,
+    photo: url,
+    location: { coordinates: [latitude, longitude] }
+  })
+    .then(moment => {
+      res.redirect(`/moment/${moment._id}`);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - > ALL MOMENTS
 momentRouter.get('/view-all', (req, res, next) => {
@@ -66,6 +54,27 @@ momentRouter.get('/view-all', (req, res, next) => {
       next(error);
     });
 });
+
+//-----------------------------------------SEARCH FOR NEARBY MOMENTS----------------------
+//onst metersToDegrees = meters => meters / ((1000 * 40000) / 360);
+//
+//omentRouter.get('/search', (req, res, next) => {
+// const latitude = req.query.latitude;
+// const longitude = req.query.longitude;
+// Moment.find()
+//   .where('location')
+//   .within()
+//   .circle({
+//     center: [longitude, latitude],
+//     radius: metersToDegrees(10000)
+//   })
+//   .then(moments => {
+//     res.render('moment/search', { moments });
+//   })
+//   .catch(error => {
+//     next(error);
+//   });
+//});
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - > SINGLE MOMENT
 
@@ -105,9 +114,9 @@ momentRouter.post('/:id/edit', (req, res, next) => {
     description: data.description,
     learning: data.learning,
     gratitude: data.gratitude,
-    photo: data.photo
-    // latitude: data.coordinates.latitude[0],
-    // longitude: data.coordinates.longitude[1]
+    photo: data.photo,
+    latitude: data.coordinates.latitude[0],
+    longitude: data.coordinates.longitude[1]
   })
     .then(() => {
       res.render('/');
