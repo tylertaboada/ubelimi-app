b38c4ced006725353825a755c5303d10159a0d96
Tyler Taboada <tjy5008@gmail.com>
2020-08-06 09:21:38 +0100

    css changes to user.hbs

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,19 @@ router.get('/about', (req, res, next) => {
   res.render('about');
 });
 
+// const button = document.getElementByClassName('.container button');
+// const jokeText = document.getElementByClassName('.container p');
+
+// button.addEventListener('click', getJoke);
+
+// async function getJoke() {
+//   const jokeData = await fetch('https://icanhazdadjoke.com/', {
+//     headers: {
+//       Accept: 'application/json'
+//     }
+//   });
+//   const jokeObj = await jokeData.json();
+//   jokeText.innerHTML = jokeObj.joke;
+// }
+
 module.exports = router;
